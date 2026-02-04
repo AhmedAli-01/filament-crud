d58c05bfa0f9b0ac58333537c2caa79478aefe66
AhmedAli-01 <15903693+AhmedAli-01@users.noreply.github.com>
2026-02-04 21:22:10 +0000

    feat: configure Post form and table with eager loading

---
+++ app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Filament\Resources\Posts\Schemas;
+
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Schema;
+
+class PostForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                Select::make('category_id')
+                    ->relationship('category', 'name')
+                    ->required(),
+                TextInput::make('title')
+                    ->required(),
+                Textarea::make('content')
+                    ->required()
+                    ->columnSpanFull(),
+                Toggle::make('is_published')
+                    ->required(),
+            ]);
+    }
+}
